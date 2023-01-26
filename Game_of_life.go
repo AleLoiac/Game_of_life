fe18daf5c410c6f84efbb7229f85682f2a2f7e46
AleLoiac <alessandroloiac@gmail.com>
2023-01-26 20:41:18 +0100

    26/01

--- Game_of_life.go
+++ Game_of_life.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 func main() {
+
+	var dimension int
+	var seed int64
+	fmt.Scan(&dimension, &seed)
+
+	rand.Seed(seed)
+
+	universe := make([][]int, dimension)
+	for element := range universe {
+		universe[element] = make([]int, dimension)
+	}
+
+	for i := 0; i < dimension; i++ {
+		for j := 0; j < dimension; j++ {
+			x := rand.Intn(2)
+			universe[i][j] = x
+			if x == 0 {
+				fmt.Print(" ")
+			} else {
+				fmt.Print("O")
+			}
+		}
+		fmt.Println()
+	}
 
 }
